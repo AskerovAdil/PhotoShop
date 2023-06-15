efddc2357b5cdb951b69a0bf4b1b539c1c362ce8
AskerovAdil <Askerov121099@mail.ru>
2023-06-15 22:35:21 +0700

    feach--add-filter

---
+++ Data/Pixel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPhotoshop.Data
+{
+    public class Pixel
+    {
+        public double R;
+        public double G;
+        public double B;
+/*        public double R { 
+            get { return R; } 
+            set { 
+                if(value>0 && value<1)
+                    R= value;
+            } 
+        }
+        public double G
+        {
+            get { return R; }
+            set
+            {
+                if (value > 0 && value < 1)
+                    R = value;
+            }
+        }
+        public double B
+        {
+            get { return B; }
+            set
+            {
+                if (value > 0 && value < 1)
+                    B = value;
+            }
+        }*/
+    }
+}
