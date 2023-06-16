8ed52949eafa2ef8e850535a2cc18cd63f45acdd
AskerovAdil <Askerov121099@mail.ru>
2023-06-16 14:51:20 +0700

    add--properies-pixel

--- Data/Pixel.cs
+++ Data/Pixel.cs
@@ -8,33 +8,41 @@ namespace MyPhotoshop.Data
 {
     public class Pixel
     {
-        public double R;
-        public double G;
-        public double B;
-/*        public double R { 
-            get { return R; } 
-            set { 
-                if(value>0 && value<1)
-                    R= value;
-            } 
+
+        private double Check(double value)
+        {
+            if (value < 0 || value > 1) throw new ArgumentException();
+            return value;
         }
+
+        private double red;
+        public double R
+        {
+
+            get { return red; }
+            set
+            {
+                red = Check(value);
+            }
+        }
+        private double green;
         public double G
         {
-            get { return R; }
+            get { return green; }
             set
             {
-                if (value > 0 && value < 1)
-                    R = value;
+                green = Check(value);
             }
         }
+
+        private double blue;
         public double B
         {
-            get { return B; }
+            get { return blue; }
             set
             {
-                if (value > 0 && value < 1)
-                    B = value;
+                blue = Check(value);
             }
-        }*/
+        }
     }
 }
